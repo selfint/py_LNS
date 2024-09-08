6820c615f04ac130ed9c9ed46ec872de7ca62105
Gilad Levy <hagiladush@gmail.com>
2024-09-08 12:00:44 +0300

    implemented Agent loading

--- aux.py
+++ aux.py
@@ -2,5 +2,5 @@ def extract_digits(s:str):
     return int(''.join(c for c in s if c.isdigit()))
 
 def bin_to_char(digit):
-    map = {0: '.', 1: '@'}
+    map = {0: '.', 1: '@', 2: 'S', 3:'G'}
     return map[digit]
--- instance.py
+++ instance.py
@@ -3,8 +3,9 @@ import numpy as np
 
 
 class instance:
-    def __init__(self, map_f_name, verbose = True):
+    def __init__(self, map_f_name,agent_fname, verbose = True):
         self.map_f_name = map_f_name
+        self.agent_fname = agent_fname
         self.verbose = verbose
     def load_map(self):
         with open(self.map_f_name) as map_file:
@@ -16,9 +17,40 @@ class instance:
         file_string = [c for c in file_string if c != '\n']
         self.map = np.array(list(map(lambda c: int(c != '.'), file_string))).reshape(self.num_of_rows, self.num_of_cols)
         if self.verbose:
-            print(f'**** Successfully loaded map from: {self.map_f_name} ****')
+            print(f'\n**** Successfully loaded map from: {self.map_f_name} ****')
             print(f'     number of rows: {self.num_of_rows}')
-            print(f'     number of columns: {self.num_of_cols}')
+            print(f'     number of columns: {self.num_of_cols}\n')
     def print_map(self):
         for line in self.map:
             print(''.join(aux.bin_to_char(num) for num in line))
+
+    def print_map_with_agents(self):
+        map = self.map.copy()
+        for loc in zip(self.start_locations, self.goal_locations):
+            print(loc)
+            map[loc[0][0], loc[0][1]] = 2
+            map[loc[1][0], loc[1][1]] = 3
+
+        for line in map:
+            print(''.join(aux.bin_to_char(num) for num in line))
+
+    def print_agents(self):
+        map = np.zeros_like(self.map)
+        for loc in zip(self.start_locations, self.goal_locations):
+            print(loc)
+            map[loc[0][0], loc[0][1]] = 2
+            map[loc[1][0], loc[1][1]] = 3
+
+        for line in map:
+            print(''.join(aux.bin_to_char(num) for num in line))
+
+    def load_agents(self):
+        with open(self.agent_fname) as agent_file:
+            lines = agent_file.readlines()[1:]
+        lines = [line.split('\t')[4:-1] for line in lines]
+        lines = [[int(num) for num in line] for line in lines]
+        self.start_locations = [line[:2] for line in lines]
+        self.goal_locations = [line[2:] for line in lines]
+        self.num_agents = len(self.start_locations)
+        if self.verbose:
+            print(f'**** Successfully loaded {self.num_agents} agents! ****\n')

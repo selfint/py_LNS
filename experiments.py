a863df2a23126d33dd28d5b83dadae05c65cf2dd
Tom Selfin <selfint@gmail.com>
2024-11-21 13:08:23 +0200

    implement optimistic iteration experiment

--- experiments.py
+++ experiments.py
@@ -1,6 +1,8 @@
+from typing import TypedDict
 import instance
 from logger import Logger
 import solvers
+import LowLevelSolvers
 from PathTable import PathTable
 import os
 import pandas as pd
@@ -118,7 +120,7 @@ def destroy_method_ablation_exp(map_path, agent_path, solver_name, verbose = Tru
 
 
 def test_exp(map_path, agent_path, solver_name, verbose = True, n_paths = 3, temp = 1):
-    #open('test.csv', "a")
+    # open('test.csv', "a")
     group_sizes = [20]#range(1, 12)
     solvers_list = ['pp','rank-pp']#range(1, 12)
     variants = list(itertools.product(group_sizes, solvers_list))
@@ -131,8 +133,8 @@ def test_exp(map_path, agent_path, solver_name, verbose = True, n_paths = 3, tem
         adj_matrix = t.get_collisions_matrix(s.num_agents)
         largest_cc = get_largest_connected_component(adj_matrix)
         random_walk_until_neighborhood_is_full(adj_matrix, largest_cc, subset_size=5)
-        #print(t.get_collisions_matrix(s.num_agents).sum(axis=1))
-        #t.get_agent_collisions_for_paths(s.agents[2], s.num_agents)
+        # print(t.get_collisions_matrix(s.num_agents).sum(axis=1))
+        # t.get_agent_collisions_for_paths(s.agents[2], s.num_agents)
         solver = solvers.IterativeRandomLNS(s, t, size, destroy_method_name='cc',low_level_solver_name = solver_name, num_iterations= 3000)
         x_axis = range(solver.num_iterations + 1)
         solver.run()
@@ -143,3 +145,100 @@ def test_exp(map_path, agent_path, solver_name, verbose = True, n_paths = 3, tem
     y_axis_label = 'collision counts'
     y_labels = [f'subset size = {size}, solver = {solver}' for size, solver in variants]
     plot_line_graphs(x_axis, collision_counts, label, x_axis_label,y_axis_label, y_labels)
+
+
+class OptimisticIterationResult(TypedDict):
+    total_colliding_agents: int
+    total_improved_agents: int
+    total_improvement: int
+
+
+def optimistic_iteration_exp(
+    map_path, agent_path, n_paths=3, temp=1, verbose=True, n_iterations=1000
+) -> list[OptimisticIterationResult]:
+    inst = instance.instance(
+        map_path,
+        agent_path,
+        n_paths=n_paths,
+        instance_name="Optimistic Iteration",
+        agent_path_temp=temp,
+        verbose=verbose
+    )
+
+    table = PathTable(inst.num_of_rows, inst.num_of_cols)
+
+    group_size = 20
+
+    solver = solvers.IterativeRandomLNS(inst, table, group_size, num_iterations=n_iterations)
+
+    solvers.random_initial_solution(inst, table)
+
+    results: list[OptimisticIterationResult] = []
+
+    for iteration in range(n_iterations):
+        # get improvement from changing a single agent
+        total_colliding_agents = 0
+        total_improved_agents = 0
+        total_improvement = 0
+        for agent_id, agent in inst.agents.items():
+            initial_path = agent.path_id
+
+            assert initial_path != -1, "got agent without path"
+
+            # get amount of robots this robot collides with
+            collision_matrix = table.get_collisions_matrix(inst.num_agents)
+            initial_collisions = collision_matrix[agent_id].sum()
+
+            # ignore robots without collisions
+            if initial_collisions == 0:
+                continue
+
+            total_colliding_agents += 1
+
+            # iterate over all other path selections
+            table.remove_path(agent_id, agent.paths[initial_path])
+            best_collisions = initial_collisions
+            for path_id in range(inst.n_paths):
+                if path_id == initial_path:
+                    continue
+
+                table.insert_path(agent_id, agent.paths[path_id])
+
+                iteration_collision_matrix = table.get_collisions_matrix(
+                    inst.num_agents
+                )
+                new_collisions = iteration_collision_matrix[agent_id].sum()
+                if new_collisions < initial_collisions:
+                    best_collisions = initial_collisions
+                    if verbose:
+                        print(f'\n      Found new path selection: {path_id} ****')
+                        print(f'\n      New number of collisions: {best_collisions} ****')
+
+                table.remove_path(agent_id, agent.paths[path_id])
+
+            collision_reduction = initial_collisions - best_collisions
+            if collision_reduction > 0:
+                total_improved_agents += 1
+                total_improvement += collision_reduction
+
+            # restore initial state
+            table.insert_path(agent_id, agent.paths[initial_path])
+
+        # print results
+        print(f'\n**** Iteration {iteration} ****')
+        print(f'\nTotal colliding agents: {total_colliding_agents}')
+        print(f'\nTotal improved agents: {total_improved_agents}')
+        print(f'\nTotal improvement: {total_improvement}')
+
+        result: OptimisticIterationResult = {
+            "total_colliding_agents": total_colliding_agents,
+            "total_improved_agents": total_improved_agents,
+            "total_improvement": total_improvement,
+        }
+
+        results.append(result)
+
+        # improve with solver
+        solver.run_iteration()
+
+    return results

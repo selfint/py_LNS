c843d663024c96b0d97449cbaad5cdf09d56109e
Tom Selfin <selfint@gmail.com>
2024-11-27 13:25:04 +0200

    fix edge collisions

--- PathTable.py
+++ PathTable.py
@@ -1,7 +1,80 @@
+from typing import Generator, NamedTuple
 import itertools
 import numpy as np
 import Agent
 from collections import defaultdict
+
+
+class Vertex(NamedTuple):
+    x: int
+    y: int
+    t: int
+
+
+class Edge(NamedTuple):
+    px: int
+    py: int
+    pt: int
+    x: int
+    y: int
+    t: int
+
+    def reverse(self):
+        """
+        Given this edge:
+        (px, py, pt) -> (x, y, t)
+
+        The 'reversed' edge is:
+        (x, y, pt)   -> (px, py, t)
+
+        Meaning an agent came from my next location to my previous location,
+        in reversed timestamps (and where t - pt == 1).
+        """
+
+        assert self.t - self.pt == 1, "got invalid edge, timestamp diff > 1"
+
+        return self.x, self.y, self.pt, self.px, self.py, self.t
+
+
+def iter_vertices(path: list[tuple[int, int]]) -> Generator[Vertex, None, None]:
+    for (x, y), t in zip(path, range(len(path))):
+        yield Vertex(x, y, t)
+
+
+def iter_edges(path: list[tuple[int, int]]) -> Generator[Edge, None, None]:
+    prev = None
+    for (x, y), t in zip(path, range(len(path))):
+        if prev is not None:
+            px, py, pt = prev
+
+            yield Edge(px, py, pt, x, y, t)
+
+        prev = x, y, t
+
+
+def iter_path(path: list[tuple[int, int]]) -> Generator[tuple[Vertex, Edge | None], None, None]:
+    """
+    Yields vertices and edges of a path, where the edge is the connection
+    between the previous vertex to the current vertex.
+
+    Note:
+        The first edge is None, since there is no previous vertex for the
+        first vertex.
+
+    Args:
+        path (list[tuple[int, int]]): the path to iterate over
+
+    Yields:
+        tuple[Vertex, Edge | None]: the vertex and edge (edge from previous vertex)
+    """
+
+    vertices = iter_vertices(path)
+    edges = itertools.chain([None], iter_edges(path))
+
+    for vertex, edge in zip(vertices, edges):
+        yield vertex, edge
+
+
 class PathTable:
     """
     Represents paths in the grid.
@@ -13,9 +86,11 @@ class PathTable:
     contains the agent ids that are at location (i,j) at time t.
     """
 
-    table: defaultdict[tuple[int, int, int], set[int]]
+    table: defaultdict[Vertex, set[int]]
+    edges: defaultdict[Edge, set[int]]
     collision_matrix: np.ndarray
     num_of_collision_points: int
+    makespan: int
 
     def __init__(self, num_of_rows, num_of_cols, num_of_agents=500):
         self.table = defaultdict(set)
@@ -25,42 +100,36 @@ class PathTable:
         self.makespan = -1
 
     def insert_path(self, agent_id, path):
-        prev = None
-        for (x, y), t in zip(path, range(len(path))):
-            self.insert_point(agent_id, x, y, t)
+        for vertex, edge in iter_path(path):
+            self.insert_vertex(agent_id, vertex)
 
-            if prev is not None:
-                px, py, pt = prev
-                self.insert_edge(agent_id, px, py, pt, x, y, t)
+            if edge is not None:
+                self.insert_edge(agent_id, edge)
 
-            prev = x, y, t
-
-    def insert_point(self, agent_id, x, y, t):
+    def insert_vertex(self, agent_id: int, vertex: Vertex):
         # print(f"Inserting agent {agent_id} at ({x},{y}) at time {t}")
-        if self.table[x, y, t]:
-            for agent in self.table[x, y, t]:
+        if self.table[vertex]:
+            for agent in self.table[vertex]:
                 self.collisions_matrix[agent_id, agent] = 1
                 self.collisions_matrix[agent, agent_id] = 1
                 self.num_of_collision_points += 1
-        self.table[x, y, t].add(agent_id)
+        self.table[vertex].add(agent_id)
 
-    def insert_edge(self, agent_id, px, py, pt, x, y, t):
+    def insert_edge(self, agent_id: int, edge: Edge):
         """
         Note:
             Detects 'swap' collisions only, since other collisions are
             handled by insert_point.
 
-            A 'swap' collision is defined as:
+            A 'swap' collision occurs if these two edges exist:
             (px, py, pt) -> (x, y, t)
-            (px, py, t)  <- (x, y, pt)
+            (x, y, pt)   -> (px, py, t)
 
-            Where t - pt == 1
+            Meaning an agent came from my next location to my previous location,
+            in reversed timestamps (and where t - pt == 1).
         """
 
-        edge = px, py, pt, x, y, t
-        collision_edge = x, y, pt, px, py, t
-
-        assert t - pt == 1, "got invalid edge, timestamp diff > 1"
+        collision_edge = edge.reverse()
 
         self.edges[edge].add(agent_id)
 
@@ -70,51 +139,38 @@ class PathTable:
             self.num_of_collision_points += 1
 
     def remove_path(self, agent_id, path):
-        prev = None
-
-        for (x, y), t in zip(path, range(len(path))):
-            self.table[x, y, t].remove(agent_id)
-            for agent in self.table[x, y, t]:
+        for vertex, edge in iter_path(path):
+            self.table[vertex].remove(agent_id)
+            for agent in self.table[vertex]:
                 self.collisions_matrix[agent_id, agent] = 0
                 self.collisions_matrix[agent, agent_id] = 0
                 self.num_of_collision_points -= 1
 
-            if prev is not None:
-                px, py, pt = prev
-                edge = px, py, pt, x, y, t
-                collision_edge = x, y, pt, px, py, t
-
+            if edge is not None:
                 self.edges[edge].remove(agent_id)
+                collision_edge = edge.reverse()
 
                 for agent in self.edges[collision_edge]:
                     self.collisions_matrix[agent_id, agent] = 0
                     self.collisions_matrix[agent, agent_id] = 0
                     self.num_of_collision_points -= 1
 
-            prev = x, y, t
-
     def is_path_available(self, path):
-        prev = None
-        for (x, y), t in zip(path, range(len(path))):
-            if self.table[x, y, t]:
+        for vertex, edge in iter_path(path):
+            if self.table[vertex]:
                 return False
 
-            if prev is not None:
-                px, py, pt = prev
-                collision_edge = x, y, pt, px, py, t
-
-                if self.edges[collision_edge]:
-                    return False
-
-            prev = x, y, t
+            if edge is not None and self.edges[edge.reverse()]:
+                return False
 
     def count_collisions_points_along_path(self, path):
-        unique_agents = set.union(*(self.table[x, y, t] for (x, y), t in zip(path, range(len(path)))))
-        return len(unique_agents)
+        vertices = set.union(*(self.table[v] for v in iter_vertices(path)))
+        edges = set.union(*(self.edges[e.reverse()] for e in iter_edges(path)))
+
+        return len(set.union(vertices, edges))
 
     def count_collisions_points_along_existing_path(self, path):
-        unique_agents = set.union(*(self.table[x, y, t] for (x, y), t in zip(path, range(len(path)))))
-        return len(unique_agents)
+        return self.count_collisions_points_along_path(path)
 
     def num_collisions(self, num_robots = 90):
         return self.num_collisions_in_robots(num_robots)
@@ -129,23 +185,15 @@ class PathTable:
         n_paths = agent.n_paths
         matrix = np.zeros((n_paths,num_robots+1))
         for path_index, path in enumerate(agent.paths):
-            prev = None
-            for (x, y), t in zip(path, range(len(path))):
-                if self.table[x, y, t]:
-                    for colliding_agent_id in self.table[x, y, t]:
-                        if colliding_agent_id != agent.id:
-                            # print(f'**** agent {agent.id} collides with agent {colliding_agent_id}')
-                            matrix[path_index][colliding_agent_id] = 1
+            for vertex, edge in iter_path(path):
+                colliding_vertices = self.table[vertex]
+                colliding_edges = set() if edge is None else self.edges[edge.reverse()]
+                collisions = set.union(colliding_vertices, colliding_edges)
 
-                if prev is not None:
-                    px, py, pt = prev
-                    collision_edge = x, y, pt, px, py, t
-
-                    for colliding_agent_id in self.edges[collision_edge]:
-                        if colliding_agent_id != agent.id:
-                            matrix[path_index][colliding_agent_id] = 1
-
-                prev = x, y, t
+                for colliding_agent_id in collisions:
+                    if colliding_agent_id != agent.id:
+                        # print(f'**** agent {agent.id} collides with agent {colliding_agent_id}')
+                        matrix[path_index][colliding_agent_id] = 1
 
         return matrix.sum(axis = 1).astype(int).tolist()
 
--- PathTable_test.py
+++ PathTable_test.py
@@ -1,3 +1,4 @@
+from Agent import Agent
 from PathTable import PathTable
 
 
@@ -109,3 +110,79 @@ def test_path_table_remove_edge():
         [0, 0, 0]
     ])
     assert format_cmatrix_for_diff(cmatrix) == expected
+
+
+def test_collisions_along_path_vertices():
+    num_of_agents = 2
+    table = PathTable(3, 3, num_of_agents)
+    path_1 = build_path_from_grid([
+        [ 0,  1,  2],
+        [__, __,  3],
+        [__, __, __],
+    ])
+    path_2 = build_path_from_grid([
+        [ 0,  1,  2],
+        [__, __,  3],
+        [__, __, __],
+    ])
+
+    table.insert_path(1, path_1)
+
+    assert table.count_collisions_points_along_path(path_2) == 1
+
+def test_collisions_along_path_edges():
+    num_of_agents = 2
+    table = PathTable(3, 3, num_of_agents)
+    path_1 = build_path_from_grid([
+        [ 0,  1,  2],
+        [__, __,  3],
+        [__, __, __],
+    ])
+    path_2 = build_path_from_grid([
+        [__, __,  3],
+        [ 0,  1,  2],
+        [__, __, __],
+    ])
+
+    table.insert_path(1, path_1)
+
+    assert table.count_collisions_points_along_path(path_2) == 1
+
+def test_get_agent_collisions_for_paths():
+    num_of_agents = 3
+    table = PathTable(3, 3, num_of_agents)
+    table.insert_path(1, build_path_from_grid([
+        [__, __,  3],
+        [__, __,  2],
+        [__,  0,  1],
+    ]))
+    table.insert_path(2, build_path_from_grid([
+        [__, __, __],
+        [ 0,  1,  2],
+        [__, __,  3],
+    ]))
+
+    agent = Agent(instance=None, agent_id=3, start_loc=None, end_loc=None, n_paths=3)
+
+    agent.paths = [
+        # no collision
+        build_path_from_grid([
+            [__, __, __],
+            [ 3,  2, __],
+            [ 0,  1, __],
+        ]),
+        # 2 vertex collision
+        build_path_from_grid([
+            [__,  2,  3],  # collision at 3 with path 1
+            [__,  1, __],  # collision at 1 with path 2
+            [__,  0, __],
+        ]),
+        # 1 edge collision
+        build_path_from_grid([
+            [ 0,  1,  2],  # V
+            [__, __,  3],  # |- edge collision with path 1
+            [__, __, __],
+        ]),
+    ]
+
+    assert table.get_agent_collisions_for_paths(agent, num_of_agents) == [0, 2, 1]
